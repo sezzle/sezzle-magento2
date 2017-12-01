2f4130d2a79dde2447ccc0fbf7b0ead93f76c5da
Rishi Mukherjee <rishi.mukherjee@sezzle.com>
2017-12-01 13:14:37 +0530

    Created order body to send to sezzle

--- Model/Cron/MerchantData.php
+++ Model/Cron/MerchantData.php
@@ -12,6 +12,7 @@ class MerchantData
 	protected $scopeConfig;
 	protected $urlBuilder;
     protected $sezzleApi;
+    protected $orderInterface;
 
     public function __construct(
         \Magento\Sales\Model\OrderFactory $orderFactory,
@@ -21,7 +22,8 @@ class MerchantData
         \Psr\Log\LoggerInterface $logger,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Framework\UrlInterface $urlBuilder,
-        \Sezzle\Sezzlepay\Model\Api $sezzleApi
+        \Sezzle\Sezzlepay\Model\Api $sezzleApi,
+        \Magento\Sales\Api\Data\OrderInterface $orderInterface
     ) {
         $this->orderFactory = $orderFactory;
 
@@ -32,11 +34,60 @@ class MerchantData
         $this->logger = $logger;
 		$this->scopeConfig = $scopeConfig;
 		$this->urlBuilder = $urlBuilder;
-		$this->sezzleApi = $sezzleApi;
+        $this->sezzleApi = $sezzleApi;
+        $this->orderInterface = $orderInterface;
     }
 
     public function execute()
     {
-        $this->logger->info("Executing cron");
+        $this->sendOrdersToSezzle();
+    }
+
+    private function sendOrdersToSezzle() {
+        $today = date("Y-m-d H:i:s");
+        $yesterday = date("Y-m-d H:i:s", strtotime("-1 days"));
+
+        $yesterday = date('Y-m-d H:i:s', strtotime($yesterday));
+        $today = date('Y-m-d H:i:s', strtotime($today));
+        $ordersCollection = $this->orderFactory->create()->getCollection()
+            ->addFieldToFilter('status',
+                array(
+                    'eq' => 'complete',
+                    'eq' => 'processing'
+                )
+            )
+            // Get last day to today
+            ->addAttributeToFilter('created_at',
+                array(
+                    'from' => $yesterday,
+                    'to' => $today
+                )
+            )
+            ->addAttributeToSelect('increment_id');
+        $body = array();
+        foreach ($ordersCollection as $orderObj) {
+            $orderIncrementId = $orderObj->getIncrementId();
+            $order = $this->orderInterface->loadByIncrementId($orderIncrementId);
+            $payment = $order->getPayment();
+            $billing = $order->getBillingAddress();
+
+            $orderForSezzle = array(
+                'order_number' => $orderIncrementId,
+                'payment_method' => $payment->getMethod(),
+                'amount' => $order->getGrandTotal() * 100,
+                'currency' => $order->getOrderCurrencyCode(),
+                'sezzle_reference' => $payment->getLastTransId(),
+                'customer_email' => $billing->getEmail(),
+                'customer_phone' => $billing->getTelephone(),
+                'billing_address1' => $billing->getStreetLine(1),
+                'billing_address2' => $billing->getStreetLine(2),
+                'billing_city' => $billing->getCity(),
+                'billing_state' => $billing->getRegionCode(),
+                'billing_postcode' => $billing->getPostcode(),
+                'billing_country' => $billing->getCountryId()
+            );
+            array_push($body, $orderForSezzle);
+        }
+        $this->logger->debug(json_encode($body));
     }
 }
