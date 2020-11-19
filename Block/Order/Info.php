893d2007542a81856a5a4c3bc8eab5823d4e2c8a
Arijit De <arijit.de@sezzle.com>
2020-11-19 15:12:36 +0530

    PLAT-525:Sezzle Order Check for frontend Reference ID show

--- Block/Order/Info.php
+++ Block/Order/Info.php
@@ -19,13 +19,23 @@ class Info extends \Magento\Sales\Block\Order\Info
      */
     protected $_template = 'Sezzle_Sezzlepay::order/sezzle_order_reference.phtml';
 
+    /**
+     * Get Sezzle Order Reference ID
+     *
+     * @return string[]
+     */
     public function getSezzleOrderReferenceID()
     {
         return $this->getOrder()->getPayment()->getAdditionalInformation(Sezzle::ADDITIONAL_INFORMATION_KEY_REFERENCE_ID);
     }
 
+    /**
+     * Check if current order is Sezzle Order
+     *
+     * @return bool
+     */
     public function isSezzleOrder()
     {
-        return $this->getOrder()->getPayment()->getMethod();
+        return $this->getOrder()->getPayment()->getMethod() == Sezzle::PAYMENT_CODE;
     }
 }
