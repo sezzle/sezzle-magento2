5c2eb1d1663d6ea6e020b645e59bd776a2cf7d00
German Vargas <german.vargas@sezzle.com>
2025-10-15 15:14:41 -0500

    sezzle-express-button created

---
+++ view/frontend/web/js/express-checkout/sezzle-express-button.js
@@ -0,0 +1,54 @@
+/**
+ * @category    Sezzle
+ * @package     Sezzle_Sezzlepay
+ * @copyright   Copyright (c) Sezzle (https://www.sezzle.com/)
+ */
+define([
+  "jquery",
+  "Magento_Checkout/js/model/error-processor",
+  "Magento_Checkout/js/model/full-screen-loader",
+  "expressCheckoutSDK",
+], function ($, errorProcessor, fullScreenLoader) {
+  "use strict";
+
+  return function (clientConfig) {
+    var checkoutSDK = new Checkout(
+      clientConfig.rendererComponent.getSDKConfig()
+    );
+
+    checkoutSDK.renderSezzleButton(clientConfig.sezzleButtonContainerElementID);
+    checkoutSDK.init({
+      onClick: function (event) {
+        event.preventDefault();
+        checkoutSDK.openModal();
+        clientConfig.rendererComponent.validateCheckout().done(function () {
+          fullScreenLoader.startLoader();
+          clientConfig.rendererComponent
+            .beforeOnClick()
+            .done(function (response) {
+              var jsonResponse = $.parseJSON(response);
+              checkoutSDK.startCheckout({
+                checkout_url: jsonResponse.checkout_url,
+              });
+            })
+            .fail(function (response) {
+              errorProcessor.process(response, this.messageContainer);
+              checkoutSDK.closeModal();
+            })
+            .always(function () {
+              fullScreenLoader.stopLoader();
+            });
+        });
+      },
+      onComplete: function () {
+        clientConfig.rendererComponent.afterOnComplete();
+      },
+      onCancel: function () {
+        clientConfig.rendererComponent.catchOnCancel();
+      },
+      onFailure: function () {
+        clientConfig.rendererComponent.catchOnFailure();
+      },
+    });
+  };
+});
